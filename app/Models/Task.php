9dfbfb1ad42e6cdcb8365c67331cdae8d0c194c2
Yaroslav-Yurev <arklaid11@gmail.com>
2025-05-12 21:10:44 +0300

    when adding tasks, the position is calculated from the authorized user. task positions are rebuilt when one of the tasks is deleted

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Task extends Model
 {
@@ -21,8 +22,18 @@ class Task extends Model
     protected static function booted()
     {
         static::creating(function ($task) {
-            $max = static::max('position');
-            $task->position = is_null($max) ? 0 : $max + 1;
+            if (auth()->check()) {
+                $max = static::where('user_id', auth()->id())->max('position');
+                $task->position = is_null($max) ? 0 : $max + 1;
+            }
+        });
+
+        static::deleting(function ($deletedTask) {
+            static::where('user_id', $deletedTask->user_id)
+                ->where('position', '>', $deletedTask->position)
+                ->update([
+                    'position' => DB::raw('position - 1')
+                ]);
         });
     }
 }
